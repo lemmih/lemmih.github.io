c2e677d2eb28dc79cb8fe95852feaec73311ce11
David Himmelstrup <lemmih@gmail.com>
2018-07-25 11:25:45 +0200

    Fix deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
-cd _site/ && git status
-cd _site/ && git add --all
-cd _site/ && git commit -m "Update (`date '+%F %T %Z'`) [ci skip]"
-cd _site/ && git push origin master
+#!/usr/bin/env bash
+
+(cd _site/ && git status)
+(cd _site/ && git add --all)
+(cd _site/ && git commit -m "Update (`date '+%F %T %Z'`) [ci skip]")
+(cd _site/ && git push origin master)
 git status
 git add _site/
 git commit -m "Update _site (`date '+%F %T %Z'`) [ci skip]"

d5f0bd04f543dc6bd3cfb213377d8c64417d50cb
David Himmelstrup <lemmih@gmail.com>
2018-07-25 11:24:41 +0200

    Deploy script.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+cd _site/ && git status
+cd _site/ && git add --all
+cd _site/ && git commit -m "Update (`date '+%F %T %Z'`) [ci skip]"
+cd _site/ && git push origin master
+git status
+git add _site/
+git commit -m "Update _site (`date '+%F %T %Z'`) [ci skip]"
+git push origin hakyll

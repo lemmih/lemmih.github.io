03acf8da228284a354526df3e5e44e6d02f7cdc6
David Himmelstrup <lemmih@gmail.com>
2018-07-25 11:28:34 +0200

    Wibbles.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+stack build && stack exec site build
 (cd _site/ && git status)
 (cd _site/ && git add --all)
 (cd _site/ && git commit -m "Update (`date '+%F %T %Z'`) [ci skip]")
